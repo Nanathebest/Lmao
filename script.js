568d753c14dc091851d20d896e700349eb912b29
Nanathebest <xerohoshi@gmail.com>
2024-11-09 03:12:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "HAHA gay boy😂🤣";
+  gif.src =
+    "https://media.giphy.com/media/2aHPr6w2qrsxa/giphy.gif?cid=790b7611k4fke0qyqccgykaahzvpd2kjcwf3wnnpygwipo5a&ep=v1_gifs_search&rid=giphy.gif&ct=g";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
